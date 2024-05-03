c85381301e2b849f9d0b760a867122d83a7fc35b
Omg <80164990+omg-urt@users.noreply.github.com>
2024-05-03 16:03:25 +0000

    Update cl_main.c
    Until i fix my local branch here a fast patch so u can have cl_guidServerUniq 1 without the qvm overwriting it.

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -4152,7 +4152,7 @@ void CL_Init( void ) {
 
 	cl_lanForcePackets = Cvar_Get( "cl_lanForcePackets", "1", CVAR_ARCHIVE_ND );
 
-	cl_guidServerUniq = Cvar_Get( "cl_guidServerUniq", "1", CVAR_ARCHIVE_ND );
+	cl_guidServerUniq = Cvar_Get( "cl_guidServerUniq", "1", CVAR_ARCHIVE_ND | CVAR_PROTECTED );
 
 	cl_dlURL = Cvar_Get( "cl_dlURL", "http://urt.li/q3ut4/%1.pk3", CVAR_ARCHIVE_ND );
 
